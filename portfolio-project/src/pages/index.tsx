7e125f63e49467a0064c9f18757fa909e87f1839
SvenH01 <sbhoving@gmail.com>
2023-07-07 22:27:17 +0200

    added parallax effect

--- portfolio-project/src/pages/index.tsx
+++ portfolio-project/src/pages/index.tsx
@@ -1,10 +1,18 @@
-import { type NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { api } from "~/utils/api";
+import {type NextPage} from "next";
 import Navbar from "~/components/Navbar";
 import bg_mobile from '~/assets/background_blob_mobile.svg'
 import bg from '~/assets/background_blob.svg'
-import type {CSSProperties} from "react";
+import foto_me from '~/assets/foto_me.jpeg'
+import python from '~/assets/tech_stack/python.svg'
+import mongo from '~/assets/tech_stack/mongo.svg.png'
+import nextjs from '~/assets/tech_stack/nextjs.svg'
+import react from '~/assets/tech_stack/react.svg.png'
+import java from '~/assets/tech_stack/java.png'
+import gratuation_photo from '~/assets/gratuation_photo.jpeg'
+import type {CSSProperties, MutableRefObject} from "react";
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import {useRef} from "react";
+import Image from "next/image";
 
 const default_background_style: CSSProperties = {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/restrict-template-expressions
@@ -17,7 +25,7 @@ const default_background_style: CSSProperties = {
     animation: "resize-intro",
     animationDuration: "4s",
     maxWidth: "100vw",
-    overflow: "hidden"
+    overflow: "hidden",
 }
 
 const mobile_background_style: CSSProperties = {
@@ -31,87 +39,106 @@ const mobile_background_style: CSSProperties = {
     animation: "resize-intro",
     animationDuration: "4s",
     maxWidth: "100vw",
-    overflow: "hidden"
+    overflow: "hidden",
 }
 
 
-
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+    const parallax: MutableRefObject<IParallax> = useRef<IParallax>(null!)
 
-  return (
-    <main>
-      <div className="min-h-screen max-h-screen min-w-full max-w-screen-md overflow-hidden">
-          <div style={default_background_style} className={"hidden sm:block"} id={'main-background'}/>
-          <div style={mobile_background_style} className={"sm:hidden block"} id={'mobile-background'}/>
-          <div id={'main-content overflow-hidden'}>
-              <Navbar/>
-              <div className="container flex flex-col min-w-full items-center justify-center gap-12 px-4 py-16 ">
-                  <h1 className="text-5xl font-medium text-center py-40 md:px-40 tracking-tight sm:text-[5rem] animate-fade-up animate-duration-[6000ms]">
-                      <span className="text-blue-800">Sven Hoving</span><br/>Software Developer
-                  </h1>
-                  <div className="flex">
-                      <p className="font-Josefin font-semibold text-2xl text-center py-4">
+    return (
+        <>
+            <Navbar/>
+            <main>
+                <Parallax ref={parallax} pages={2}>
+                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return */}
+                    <ParallaxLayer speed={1} factor={1} onClick={() => parallax.current.scrollTo(0.34)} offset={0} className="overflow-hidden">
+                        <div style={default_background_style} className={"hidden sm:block"} id={'main-background'}/>
+                        <div style={mobile_background_style} className={"sm:hidden block"} id={'mobile-background'}/>
+                        <div id={'main-content overflow-hidden'}>
+                            <div className="container flex flex-col min-w-full items-center justify-center gap-12 px-4 py-16 ">
+                                <h1 className="text-5xl font-medium text-center py-40 md:px-40 tracking-tight sm:text-[5rem] animate-fade-up animate-duration-[6000ms]">
+                                    <span className="text-blue-800">Sven Hoving</span><br/>Software Developer
+                                </h1>
+                            </div>
+                        </div>
+                    </ParallaxLayer>
+                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return */}
+                    <ParallaxLayer speed={2} offset={0.99}  factor={1} className={"bg-[#0066FF]"} onClick={() => parallax.current.scrollTo(1)}>
+                        <div className={"flex-none sm:flex p-10 gap-20"}>
+                            <div className={"sm:flex-1 flex-none align-top text-center"}>
+                                <h1 className="font-Oswald text-white">About me</h1>
+                            </div>
+                            <div className={"sm:flex-1 flex-none"}>
+                                <p className={"text-white bottom-0 pt-10"}>
+                                    I graduated HBO-ICT:Software Engineering in 4 years at the University of Applied Sciences in Amsterdam. At the HvA I learned about concepts such as OOP, Databases, Webframeworks and Algorithms & Datastructures. Besides these necessary concepts I have also worked in different project groups where I learned to apply my professional skills in a SCRUM based context.
+                                </p>
+                                <Image className={"flex pt-10 justify-self-center"} width={400} height={400} src={gratuation_photo.src} alt={"photo of Sven Hoving"}></Image>
+                            </div>
+                        </div>
 
-                      </p>
-                  </div>
-                  {/*<Link*/}
-                  {/*  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4"*/}
-                  {/*  href="/cv"*/}
-                  {/*  // target="_blank"*/}
-                  {/*>*/}
-                  {/*  <h3 className="text-2xl font-bold">Curriculum vitae</h3>*/}
-                  {/*  <div className="text-lg font-extralight">*/}
-                  {/*    As you explore this page, you&apos;ll discover my extensive background in software development, along with my proficiency in a wide range of programming languages and technologies. I have a solid foundation in software engineering principles and gained experience by my internships, hobby projects and as full-stack developer.*/}
-                  {/*  </div>*/}
-                  {/*</Link>*/}
-                  {/*<div*/}
-                  {/*  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4"*/}
-                  {/*>*/}
-                  {/*  <h3 className="text-2xl font-bold">Learning goals and competences</h3>*/}
-                  {/*  <div className="text-lg font-extralight">*/}
-                  {/*    In the coming years I will study mathematics, various AI topics and keep my front-end knowledge up to date. On this page, my progress will be shown for each topic, and my learning goals for every month will be displayed.*/}
-                  {/*  </div>*/}
-                  {/*</div>*/}
-                  {/*</div>*/}
-                  {/*<div className="flex flex-col items-center gap-2">*/}
-                  {/*  /!*<p className="text-2xl">*!/*/}
-                  {/*  /!*  {hello.data ? hello.data.greeting : "Loading tRPC query..."}*!/*/}
-                  {/*  /!*</p>*!/*/}
-                  {/*  /!*<AuthShowcase />*!/*/}
-                  {/*</div>*/}
-              </div>
-          </div>
-      </div>
-        <div className="flex min-h-screen flex-col bg-[#0066FF]">
-            <h1 className="font-Oswald text-white text-center">Work in progress...</h1>
-        </div>
-    </main>
-  );
+                        <div className={"flex-none sm:flex sm:flex-row-reverse p-10 gap-20 bg-[#0066FF]"} >
+                            <div className={"sm:flex-1 flex-none align-top text-center"}>
+                                <h1 className="font-Oswald text-white ">Work experience</h1>
+                            </div>
+                            <div className={"sm:flex-1 flex-none"}>
+                                <p className={"text-white bottom-0 pt-10"}>
+                                    I gained working experience trough various internships at companies such as Foleon, Youplan and Mud Jeans.                                </p>
+                            </div>
+                        </div>
+                    </ParallaxLayer>
+                    {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return */}
+                    <ParallaxLayer offset={1} className={"-z-10"} >
+                        <h1 className="font-Oswald text-center">Tech stack</h1>
+                        <div className={"flex flex-row justify-center align-middle h-screen"}>
+                        <div>
+                            <Image width={100} height={100} src={react.src} alt={"React logo"}/>
+                        </div>
+                        <div>
+                            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */}
+                            <Image width={100} height={100} src={nextjs.src} alt={"NextJS logo"}/>
+                        </div>
+                        <div>
+                            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access */}
+                            <Image width={100} height={100} src={python.src} alt={"Python logo"}/>
+                        </div>
+                        <div>
+                            <Image width={100} height={100} src={java.src} alt={"Java logo"}/>
+                        </div>
+                        <div>
+                            <Image width={200} height={200} src={mongo.src} alt={"Mongo logo"}/>
+                        </div>
+                        </div>
+
+                    </ParallaxLayer>
+                </Parallax>
+            </main>
+        </>
+    );
 };
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
+// const AuthShowcase: React.FC = () => {
+//     const {data: sessionData} = useSession();
+//
+//     const {data: secretMessage} = api.example.getSecretMessage.useQuery(
+//         undefined, // no input
+//         {enabled: sessionData?.user !== undefined},
+//     );
+//
+//     return (
+//         <div className="flex flex-col items-center justify-center gap-4">
+//             <p className="text-center text-2xl text-white">
+//                 {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+//                 {secretMessage && <span> - {secretMessage}</span>}
+//             </p>
+//             <button
+//                 className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+//                 onClick={sessionData ? () => void signOut() : () => void signIn()}
+//             >
+//                 {sessionData ? "Sign out" : "Sign in"}
+//             </button>
+//         </div>
+//     );
+// };
